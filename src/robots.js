529a2e326b10d899d54a3b974080dc00e2f1067f
morar03 <morarcristiantraian@gmail.com>
2020-12-16 21:57:47 +0200

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Lea1nne Graha1m',
+	username: 'Br1et',
+	email : 'te1st@test.com'
+},
+{
+	id: 2,
+	name: 'Leann2e Grah2am',
+	username: 'Br2et',
+	email : 'tes2t@test.com'
+},
+{
+	id: 3,
+	name: 'Lea3nne Graha3m',
+	username: 'Br3et',
+	email : 'te3st@test.com'
+},
+{
+	id: 4,
+	name: 'Lean34ne Gra34ham',
+	username: 'B4ret',
+	email : 'tes4t@test.com'
+},
+{
+	id: 5,
+	name: 'Lean5ne Grah5am',
+	username: 'Br5et',
+	email : 'tes5t@te5st.com'
+},
+{
+	id: 6,
+	name: 'Leann6e Graham',
+	username: 'Br6et',
+	email : 'tes6t@test.com'
+},
+{
+	id: 7,
+	name: 'Lea7nne Graham',
+	username: 'B7ret',
+	email : 'tes7t@test.com'
+},
+{
+	id: 8,
+	name: 'Lea8nne Graham',
+	username: 'Br8et',
+	email : 'tes8t@test.com'
+},
+];
